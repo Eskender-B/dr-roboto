84d5ec2b0332a05fbaa15ad628b8673f563a672c
Eskender <eskenderbesrat@gmail.com>
2018-03-15 19:08:50 +0300

    Add test rules for sample detector functions

--- behavior/functions.scm
+++ behavior/functions.scm
@@ -1,6 +1,5 @@
-;;;; These are functions used by Ghost or OpenPsi rules
+;;;; These file contains functions used by Ghost or OpenPsi rules
 ;;;; Rule is like	context ==> action
-
 
 ;; Functions for context part
 ;; Need to return either (stv 0 1) or (stv 1 1) which are like false and true
@@ -16,7 +15,7 @@
 )
 
 
-;; Functions for context part
+;; Functions for action part
 ;; Need to return word nodes in a listlink
 ;; They need also be grounded functions
 
@@ -27,6 +26,36 @@
   ; The answer should be a list of nodes wrapped in a ListLink
   (List (Word "Bob") (Word "and") (Word "Alice")))
 
+
 (Define
-  (DefinedSchema "findkiller")
-  (Lambda (ExecutionOutput (GroundedSchema "scm: findkiller") (List))))
+  (DefinedSchema "func-findkiller")
+  (Lambda (ExecutionOutput (GroundedSchema "scm: func-findkiller") (List))))
+
+
+
+; Sample returning detector function
+(define-public (func-returning-result)
+	
+	(List (Word (number->string(detector-returning-func))))
+)
+
+(Define
+	(DefinedSchema "func-returning-result")
+	(Lambda (ExecutionOutput (GroundedSchema "scm: func-returning-result") (List)))
+)
+
+
+; Sample looping detector function
+(define-public (func-looping-result)
+	; Check first if result did not expire
+	(if (< (- (current-time) (hash-ref detector-looping-func-hashT "time"))  5)
+		(List (Word (number->string
+				(hash-ref detector-looping-func-hashT "val"))))
+		(List (Word " "))
+	)
+)
+
+(Define
+	(DefinedSchema "func-looping-result")
+	(Lambda (ExecutionOutput (GroundedSchema "scm: func-looping-result") (List)))
+)
