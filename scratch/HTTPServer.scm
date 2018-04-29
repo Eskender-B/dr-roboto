5a16761319217d1d9dc6f4f62e9ee9f9a5e13741
Eskender <eskenderbesrat@gmail.com>
2018-04-29 13:28:31 +0300

    Make scratch server respond to crossdoman.xml reset_all

--- scratch/HTTPServer.scm
+++ scratch/HTTPServer.scm
@@ -24,6 +24,23 @@
 	(string-append "speechOutput " "Dummy output\n")
 )
 
+(define (send-cross-domain-policy)
+	(string-append "<cross-domain-policy>"
+				"<allow-access-from-domain=\"*\" to-ports=\"<7777>\"/>"
+			"</cross-domain-policy>"
+	)
+)
+
+
+; Function that executes when scratch program starts (green flag clicked)
+; Does nothing for the time being.
+(define (reset-all)
+	"okay"	
+)
+
+
+
+
 ; Implement HTTP Handler
 (define (scratch-handler request request-body)
 	
@@ -33,13 +50,19 @@
 	(define command
 			(split-and-decode-uri-path (uri-path (request-uri request))))
 
+
 	(define resp
 		(cond
 			((equal? (list-ref command 0) "speechInput") (speech-input (list-ref command 1)))
 			((equal? (list-ref command 0) "ghostRule") (ghost-rule (list-ref command 1))  )
 			((equal? (list-ref command 0) "poll") (speech-output) )
+			((equal? (list-ref command 0) "crossdomain.xml") (send-cross-domain-policy))
+			((equal? (list-ref command 0) "reset_all") (reset-all))
 		)
 	)
+
+	;(display command)
+	;(newline)
 	
 	(values 
 		'((content-type . (text/html)))
